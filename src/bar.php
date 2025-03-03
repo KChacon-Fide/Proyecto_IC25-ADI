8d1046251134703b6bbf04baf5adfb5cd7a755c3
Alexander <jabarca20358@ufide.ac.cr>
2025-03-02 20:21:58 -0500

    bar diseno actualizado

--- src/bar.php
+++ src/bar.php
@@ -5,40 +5,46 @@ if (empty($_SESSION['active'])) {
     exit();
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Panel de Bar</title>
     <link rel="stylesheet" href="../assets/plugins/fontawesome-free/css/all.min.css">
     <link rel="stylesheet" href="../assets/dist/css/adminlte.min.css">
-    <link rel="stylesheet" href="../assets/dist/css/Bar.css"> 
+    <link rel="stylesheet" href="../assets/plugins/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="../assets/dist/css/Bar.css">
+
+    
+    
 </head>
 
 <body>
-    <!-- Botón de expandir pantalla -->
-    <div class="expand-button" onclick="toggleFullscreen()">
-        <i id="expand-icon" class="fas fa-expand-arrows-alt"></i>
-    </div>
+    
 
     <!-- Botón de regreso -->
-    <img src="../assets/img/return.png" alt="Volver al Dashboard" class="return-button" onclick="location.href='dashboard.php';">
+    <img src="../assets/img/return.png" alt="Volver al Dashboard" class="return-button"
+        onclick="location.href='dashboard.php';">
 
-    <div class="wrapper">
-        <div class="content-wrapper">
-            <section class="content">
-                <h2 class="text-center">Órdenes de Bar Pendientes</h2>
-                <div id="pedidos-container">
-                    <!-- Aquí se insertarán las órdenes dinámicamente -->
-                </div>
-            </section>
+    <div class="container mt-4">
+        <h2  class="fw-bold">ESTADO DEL BAR </h2>
+        <div id="botonesFiltro" class="d-flex gap-2 mb-4">
+        </div>
+        <hr>
+
+        <div id="pedidos-container" class="row g-3">
+
         </div>
     </div>
 
     <script src="../assets/plugins/jquery/jquery.min.js"></script>
     <script src="../assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="../assets/dist/js/adminlte.min.js"></script>
-    <script src="../assets/js/bar.js"></script>  
+    <script src="../assets/js/bar.js"></script>
+    <script src="../assets/plugins/fontawesome-free/js/all.min.js"></script>
+    
 </body>
+
 </html>
